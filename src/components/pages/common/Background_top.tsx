50a575df36ea53a172f9e674e78f0514ae285865
shinya watanabe <sjnrra@gmail.com>
2026-01-03 22:29:45 +0900

    top画面白とび微調整

--- src/components/pages/common/Background_top.tsx
+++ src/components/pages/common/Background_top.tsx
@@ -2,8 +2,6 @@ import { Box } from "@mui/material";
 // import React from "react";
 import React, { useEffect, useState } from "react";
 
-
-
 type BackgroundProps = {
     children: React.ReactNode;
     width?: string | number;
@@ -15,15 +13,16 @@ type BackgroundProps = {
 const images = [
     `${process.env.PUBLIC_URL}/image/top1.jpg`,
     `${process.env.PUBLIC_URL}/image/top2.jpg`,
-    `${process.env.PUBLIC_URL}/image/top3.jpg`,
+    // `${process.env.PUBLIC_URL}/image/top3.jpg`,
     `${process.env.PUBLIC_URL}/image/top10.jpg`,
     `${process.env.PUBLIC_URL}/image/top11.jpg`,
-    `${process.env.PUBLIC_URL}/image/top5.jpg`,
+    // `${process.env.PUBLIC_URL}/image/top5.jpg`,
     `${process.env.PUBLIC_URL}/image/top6.jpg`
 ];
 
 export const Background: React.FC<BackgroundProps> = ({ children }) => {
     const [index, setIndex] = useState(0);
+    const [fade, setFade] = useState(true);
 
     /******************************************
      * 背景事前ロード 
@@ -35,10 +34,21 @@ export const Background: React.FC<BackgroundProps> = ({ children }) => {
         });
     }, []);
 
+    /******************************************
+     * 背景切替＆フェード
+     ******************************************/
     useEffect(() => {
         const interval = setInterval(() => {
-            setIndex((prev) => (prev + 1) % images.length);
-        }, 4000); // 4秒ごとに切り替え
+            //フェードアウト
+            setFade(false);
+
+            setTimeout(() => {
+                setIndex((prev) => (prev + 1) % images.length);
+                //フェードイン
+                setFade(true);
+            }, 800);
+
+        }, 4000);
 
         return () => clearInterval(interval);
     }, []);
@@ -47,16 +57,23 @@ export const Background: React.FC<BackgroundProps> = ({ children }) => {
         <Box
             sx={{
                 minHeight: "100vh",
-                backgroundImage: `url(${images[index]})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                transition: "background-image 1.5s ease-in-out", // フェード効果
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
             }}
         >
+            <Box
+                sx={{
+                    position: "absolute",
+                    inset: 0,
+                    backgroundImage: `url(${images[index]})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    transition: "opacity 1s ease-in-out",
+                    opacity: fade ? 1 : 0,
+                }}
+            />
             {children}
         </Box>
     );
--- src/components/pages/top/Top.tsx
+++ src/components/pages/top/Top.tsx
@@ -5,7 +5,7 @@
 /*************************************************
  * import
  *************************************************/
-import React from "react";
+// import React from "react";
 // import Typography from '@mui/material/Typography';
 import { Box, Button, TextField, Snackbar, Alert, Grid, Typography } from "@mui/material";
 import { motion } from "framer-motion";
@@ -16,6 +16,7 @@ import styles from "css/Top.module.css";
 import Background from "components/pages/common/Background_top";
 import customiseTypography from "components/pages/common/customize_typography";
 import { ThemeProvider } from "@mui/material/styles";
+import React, { useEffect, useState } from "react";
 export const theme = customiseTypography
 
 /*************************************************
@@ -26,7 +27,7 @@ const Top: React.FC = () => {
         <>
             <Background>
                 <ThemeProvider theme={theme}>
-                {/* <div className={styles.point}>
+                    {/* <div className={styles.point}>
                 <div className={styles.box}>
                     <div className={`${styles.bgImg} ${styles.src1}`}></div>
                     <div className={`${styles.bgImg} ${styles.src2}`}></div>
@@ -36,55 +37,55 @@ const Top: React.FC = () => {
                 </div>
             </div> */}
 
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.7 }}
-                >
-
-                    <Box className={styles.top_pop12}
-                        sx={{
-                            width: {
-                                xs: "85%",   // スマホ
-                                sm: "60%",   // タブレット
-                                md: "30%",   // PC
-                                lg: "30%",   // 大画面
-                            },
-                            borderRadius: 3
-                        }}
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.7 }}
                     >
-                        <Grid>
+
+                        <Box className={styles.top_pop12}
+                            sx={{
+                                width: {
+                                    xs: "85%",   // スマホ
+                                    sm: "60%",   // タブレット
+                                    md: "30%",   // PC
+                                    lg: "30%",   // 大画面
+                                },
+                                borderRadius: 3
+                            }}
+                        >
                             <Grid>
-                                <Box
-                                    sx={{ p: 1 }} >
-                                    <div className={styles.top_pop2}>
-                                        <Typography>
-                                            <h1 className={styles.top_pop2_name1}>Watanabe Shinya</h1>
-                                            <h2 className={styles.top_pop2_name2}>渡部 晋也 作品集</h2>
-                                        </Typography>
-                                    </div>
-                                    <IconButton
-                                        color="secondary"
-                                        href="https://www.instagram.com/shinya_wata_paint?igsh=MWZqYWh0Yjk1eXc3ZQ=="
-                                        target="_blank"
-                                        rel="noopener noreferrer" // セキュリティ対策
-                                    >
-                                        <InstagramIcon fontSize="large" />
-                                    </IconButton>
-                                    <div className={styles.top_pop3}>
-                                        <Typography>
-                                            <p className={styles.top_info}>information</p>
-                                            <p className={styles.top_info_data}>2025/12/28　　新規作成</p>
-                                            <br></br><br></br><br></br>
-                                        </Typography>
-                                    </div>
-                                    {/* </div>                                     */}
-                                </Box>
+                                <Grid>
+                                    <Box
+                                        sx={{ p: 1 }} >
+                                        <div className={styles.top_pop2}>
+                                            <Typography>
+                                                <h1 className={styles.top_pop2_name1}>Watanabe Shinya</h1>
+                                                <h2 className={styles.top_pop2_name2}>渡部 晋也 作品集</h2>
+                                            </Typography>
+                                        </div>
+                                        <IconButton
+                                            color="secondary"
+                                            href="https://www.instagram.com/shinya_wata_paint?igsh=MWZqYWh0Yjk1eXc3ZQ=="
+                                            target="_blank"
+                                            rel="noopener noreferrer" // セキュリティ対策
+                                        >
+                                            <InstagramIcon fontSize="large" />
+                                        </IconButton>
+                                        <div className={styles.top_pop3}>
+                                            <Typography>
+                                                <p className={styles.top_info}>information</p>
+                                                <p className={styles.top_info_data}>2025/12/28　　新規作成</p>
+                                                <br></br><br></br><br></br>
+                                            </Typography>
+                                        </div>
+                                        {/* </div>                                     */}
+                                    </Box>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    </Box>
-                </motion.div>
+                        </Box>
+                    </motion.div>
                 </ThemeProvider>
             </Background>
         </>
